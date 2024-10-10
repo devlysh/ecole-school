0bbc8a220fbf7a1fae95cbad5b63930516361533
Artem Devlysh <artem@devlysh.com>
2024-10-11 01:34:59 +0300

    feat: implement db

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const languages = [
+    { code: "en", name: "English" },
+    { code: "es", name: "Spanish" },
+    { code: "fr", name: "French" },
+    { code: "de", name: "German" },
+    { code: "it", name: "Italian" },
+    { code: "uk", name: "Ukrainian" },
+  ];
+
+  for (const language of languages) {
+    await prisma.language.upsert({
+      where: { code: language.code },
+      update: {},
+      create: language,
+    });
+  }
+
+  console.log("Languages table populated!");
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
