0cbd9b8b6b44aeb42c5d148649f774ff548ad0d2
Artem Devlysh <artem@devlysh.com>
2024-10-11 01:35:14 +0300

    feat: add languages route

---
+++ src/app/api/v1/languages/route.ts
@@ -0,0 +1,11 @@
+import prisma from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const languages = await prisma.language.findMany();
+    return Response.json(languages);
+  } catch (error) {
+    console.error("Error fetching languages:", error);
+    return Response.json("Failed to fetch languages", { status: 500 });
+  }
+}
