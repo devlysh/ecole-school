80026d85b9b5a4d99da2b487490d4ded3c220af2
Artem Devlysh <artem@devlysh.com>
2024-10-21 02:35:07 +0300

    fix: token

--- src/app/api/v1/preregister-user/route.ts
+++ src/app/api/v1/preregister-user/route.ts
@@ -1,6 +1,7 @@
 import logger from "@/lib/logger";
-import { signToken } from "@/lib/jwt";
+import { signToken, verifyToken } from "@/lib/jwt";
 import { cookies } from "next/headers";
+import { CookiesPayload } from "@/lib/types";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
@@ -24,7 +25,7 @@ const cookiesList = [
 export const GET = async () => {
   try {
     const cookieStore = cookies();
-    const cookiesData = cookiesList.reduce(
+    let cookiesData = cookiesList.reduce(
       (data: Record<string, string>, cookieName: string) => {
         const cookie = cookieStore.get(cookieName);
         if (cookie) {
@@ -34,9 +35,21 @@ export const GET = async () => {
       },
       {}
     );
+
+    const existingToken = cookieStore.get("token");
+
+    if (existingToken) {
+      const decodedToken = (await verifyToken(
+        existingToken.value
+      )) as CookiesPayload;
+      delete decodedToken.exp;
+      cookiesData = { ...cookiesData, ...decodedToken };
+    }
+
     const token = signToken(cookiesData, { expiresIn: "1h" });
 
     const response = Response.json(token);
+
     response.headers.set(
       "Set-Cookie",
       `token=${token}; Path=/; Max-Age=${60 * 60 * 1};`
