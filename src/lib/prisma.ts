5797360cf9ae0ee4814fd42481f4323ff6a58a0f
Artem Devlysh <artem@devlysh.com>
2024-10-21 03:09:13 +0300

    fix: prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,4 +2,10 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// Ensure the Prisma Client disconnects when the application exits
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
 export default prisma;
