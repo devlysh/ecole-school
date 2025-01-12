1fca7e647cf564c587580d4781bd3f9c05f8bc24
Artem Devlysh <artem@devlysh.com>
2025-01-13 00:24:44 +0200

    test: is at permitted time strategy

--- domain/strategies/IsAtPermittedTime.strategy.test.ts
+++ domain/strategies/IsAtPermittedTime.strategy.test.ts
@@ -4,33 +4,132 @@ import {
   PermittedTimeUnit,
 } from "./IsAtPermittedTime.strategy";
 
-describe("IsInPermittedTimeStrategy", () => {
-  it("should return true for a dateTime within the permitted time range (days after)", () => {
-    const strategy = new IsAtPermittedTimeStrategy(
-      2,
-      PermittedTimeUnit.DAYS,
-      PermittedTimeDirection.AFTER,
-      new Date("2023-01-10T09:00:00Z")
-    );
+describe("IsAtPermittedTimeStrategy", () => {
+  describe("PermittedTimeDirection.AFTER", () => {
+    it("should return true for a dateTime within the permitted time range (days after)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        2,
+        PermittedTimeUnit.DAYS,
+        PermittedTimeDirection.AFTER,
+        new Date("2023-01-10T09:00:00Z")
+      );
 
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-12T09:00:00Z") })
-    ).toBe(true);
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-12T09:00:00Z") })
+      ).toBe(true);
 
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-12T10:00:00Z") })
-    ).toBe(true);
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-13T10:00:00Z") })
+      ).toBe(true);
+    });
 
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-13T10:00:00Z") })
-    ).toBe(true);
+    it("should return false for a dateTime outside the permitted time range (days after)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        2,
+        PermittedTimeUnit.DAYS,
+        PermittedTimeDirection.AFTER,
+        new Date("2023-01-10T09:00:00Z")
+      );
 
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2024-01-10T10:00:00Z") })
-    ).toBe(true);
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-09T09:00:00Z") })
+      ).toBe(false);
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-10T09:00:00Z") })
+      ).toBe(false);
+    });
+
+    it("should return true for a dateTime within the permitted time range (hours after)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        5,
+        PermittedTimeUnit.HOURS,
+        PermittedTimeDirection.AFTER,
+        new Date("2023-01-10T09:00:00Z")
+      );
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-10T14:00:00Z") })
+      ).toBe(true);
+    });
+
+    it("should return false for a dateTime outside the permitted time range (hours after)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        5,
+        PermittedTimeUnit.HOURS,
+        PermittedTimeDirection.AFTER,
+        new Date("2023-01-10T09:00:00Z")
+      );
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-10T13:00:00Z") })
+      ).toBe(false);
+    });
   });
 
-  it("should return false for a dateTime outside the permitted time range (days after)", () => {
+  describe("PermittedTimeDirection.BEFORE", () => {
+    it("should return true for a dateTime within the permitted time range (days before)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        2,
+        PermittedTimeUnit.DAYS,
+        PermittedTimeDirection.BEFORE,
+        new Date("2023-01-10T09:00:00Z")
+      );
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-07T09:00:00Z") })
+      ).toBe(true);
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-08T09:00:00Z") })
+      ).toBe(true);
+    });
+
+    it("should return false for a dateTime outside the permitted time range (days before)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        2,
+        PermittedTimeUnit.DAYS,
+        PermittedTimeDirection.BEFORE,
+        new Date("2023-01-10T09:00:00Z")
+      );
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-09T09:00:00Z") })
+      ).toBe(false);
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-10T10:00:00Z") })
+      ).toBe(false);
+    });
+
+    it("should return true for a dateTime within the permitted time range (hours before)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        5,
+        PermittedTimeUnit.HOURS,
+        PermittedTimeDirection.BEFORE,
+        new Date("2023-01-10T09:00:00Z")
+      );
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-10T04:00:00Z") })
+      ).toBe(true);
+    });
+
+    it("should return false for a dateTime outside the permitted time range (hours before)", () => {
+      const strategy = new IsAtPermittedTimeStrategy(
+        5,
+        PermittedTimeUnit.HOURS,
+        PermittedTimeDirection.BEFORE,
+        new Date("2023-01-10T09:00:00Z")
+      );
+
+      expect(
+        strategy.isAvailable({ dateTime: new Date("2023-01-10T05:00:00Z") })
+      ).toBe(false);
+    });
+  });
+
+  it("should return false if dateTime is not provided", () => {
     const strategy = new IsAtPermittedTimeStrategy(
       2,
       PermittedTimeUnit.DAYS,
@@ -38,28 +137,6 @@ describe("IsInPermittedTimeStrategy", () => {
       new Date("2023-01-10T09:00:00Z")
     );
 
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2022-01-08T09:00:00Z") })
-    ).toBe(false);
-
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-08T09:00:00Z") })
-    ).toBe(false);
-
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-09T09:00:00Z") })
-    ).toBe(false);
-
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-10T09:00:00Z") })
-    ).toBe(false);
-
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-11T09:00:00Z") })
-    ).toBe(false);
-
-    expect(
-      strategy.isAvailable({ dateTime: new Date("2023-01-12T08:00:00Z") })
-    ).toBe(false);
+    expect(strategy.isAvailable({})).toBe(false);
   });
 });
