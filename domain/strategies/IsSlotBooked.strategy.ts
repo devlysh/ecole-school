ad931969ed6486e5c7fcf7cf37decd0dbd58473e
Artem Devlysh <artem@devlysh.com>
2025-01-12 03:51:50 +0200

    fix: is slot booked strategy

--- domain/strategies/IsSlotBooked.strategy.ts
+++ domain/strategies/IsSlotBooked.strategy.ts
@@ -3,8 +3,6 @@ import {
   SlotAvailibilityContext,
   SlotAvailibilityStrategy,
 } from "./SlotAvailibilityStrategy.interface";
-import logger from "@/lib/logger";
-import { RRule } from "rrule";
 
 export class IsSlotBookedStrategy implements SlotAvailibilityStrategy {
   isAvailable(context: SlotAvailibilityContext): boolean {
@@ -13,85 +11,26 @@ export class IsSlotBookedStrategy implements SlotAvailibilityStrategy {
       return false;
     }
 
-    if (slot.rrule) {
-      return this.isAvailableWithRRule(slot, dateTime, bookedClasses);
-    } else {
-      return this.isAvailableWithoutRRule(slot, dateTime, bookedClasses);
-    }
+    return !this.isBooked(bookedClasses, slot, dateTime);
   }
 
-  private isAvailableWithRRule(
-    slot: AvailableSlot,
-    dateTime: Date,
-    bookedClasses: BookedClass[]
-  ): boolean {
-    if (!slot.rrule) {
-      return false;
-    }
-
-    try {
-      const rule = new RRule({
-        ...RRule.parseString(slot.rrule),
-        dtstart: slot.startTime,
-      });
-
-      const duration = slot.endTime.getTime() - slot.startTime.getTime();
-      const searchStart = new Date(dateTime.getTime() - duration);
-      const searchEnd = new Date(dateTime.getTime() + duration);
-      const occurrences = rule.between(searchStart, searchEnd, true);
-
-      const occurrenceMatch = occurrences.find(
-        (occ) =>
-          dateTime >= occ && dateTime < new Date(occ.getTime() + duration)
-      );
-
-      if (!occurrenceMatch) {
-        return false;
-      }
-
-      return !this.isSlotBlocked(bookedClasses, slot, dateTime);
-    } catch (err) {
-      logger.warn({ err, slot, dateTime }, "Invalid rrule");
-      return false;
-    }
-  }
-
-  private isAvailableWithoutRRule(
-    slot: AvailableSlot,
-    dateTime: Date,
-    bookedClasses: BookedClass[]
-  ): boolean {
-    if (dateTime < slot.startTime || dateTime >= slot.endTime) {
-      return false;
-    }
-    return !this.isSlotBlocked(bookedClasses, slot, dateTime);
-  }
-
-  private isSlotBlocked(
+  private isBooked(
     bookedClasses: BookedClass[],
     slot: AvailableSlot,
     dateTime: Date
   ): boolean {
-    return bookedClasses.some((bookedClass) =>
-      this.checkBooking(bookedClass, slot, dateTime)
-    );
-  }
-
-  private checkBooking(
-    bookedClass: BookedClass,
-    slot: AvailableSlot,
-    dateTime: Date
-  ): boolean {
-    if (bookedClass.teacherId !== slot.teacherId) {
-      return false;
-    }
-    if (
-      bookedClass.recurring &&
-      this.isRecurrentMatch(bookedClass.date, dateTime)
-    ) {
-      return true;
-    }
-    return bookedClass.date.getTime() === dateTime.getTime();
+    return bookedClasses.some((bookedClass) => {
+      if (bookedClass.teacherId !== slot.teacherId) {
+        return false;
+      }
+      if (
+        bookedClass.recurring &&
+        this.isRecurrentMatch(bookedClass.date, dateTime)
+      ) {
+        return true;
+      }
+      return bookedClass.date.getTime() === dateTime.getTime();
+    });
   }
 
   private isRecurrentMatch(bookedDate: Date, requestedDate: Date): boolean {
