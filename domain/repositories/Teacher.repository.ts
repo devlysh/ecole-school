892d78d60daea8d792667e4e7eaf64c55acde3ed
Artem Devlysh <artem@devlysh.com>
2025-01-14 17:38:09 +0200

    feat(repository): add methods to TeacherRepository

--- domain/repositories/Teacher.repository.ts
+++ domain/repositories/Teacher.repository.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma";
-import { Teacher } from "@prisma/client";
+import { Role } from "@/lib/types";
+import { Teacher, User } from "@prisma/client";
 
 /**
  * Example repository for getting all teachers from the DB.
@@ -12,5 +13,65 @@ export class TeacherRepository {
 
   public async findTeacherById(userId: number): Promise<Teacher | null> {
     return prisma.teacher.findUnique({ where: { userId } });
+  }
+
+  public async findTeacherByEmail(email: string): Promise<User | null> {
+    return await prisma.user.findUnique({
+      where: { email },
+      include: {
+        teacher: {
+          include: {
+            availableSlots: true,
+            vacations: true,
+          },
+        },
+      },
+    });
+  }
+
+  public async findTeachersWithRolesAndLanguages(): Promise<any[]> {
+    return prisma.user.findMany({
+      where: {
+        roles: {
+          some: {
+            role: {
+              name: Role.TEACHER,
+            },
+          },
+        },
+        teacher: {
+          isNot: null,
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        settings: true,
+        teacher: {
+          select: {
+            languages: {
+              select: {
+                language: {
+                  select: {
+                    name: true,
+                    code: true,
+                  },
+                },
+              },
+            },
+          },
+        },
+        roles: {
+          select: {
+            role: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
   }
 }
