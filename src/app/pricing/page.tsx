56a846f3d1b5c9606391da34d2e37edbcd30553a
Artem Devlysh <artem@devlysh.com>
2025-02-19 23:28:02 +0200

    fix: deploy

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -5,9 +5,6 @@ import { redirect } from "next/navigation";
 import Pricing from "@/components/Pricing";
 import { PreAuthTokenPayload, TokenType } from "@/lib/types";
 import logger from "@/lib/logger";
-import { getLanguagesRequest } from "../api/v1/languages/request";
-import { getCurrenciesRequest } from "../api/v1/currencies/request";
-import { getPlansRequest } from "../api/v1/stripe/plans/request";
 
 const PricingPage = async () => {
   const cookieStore = cookies();
